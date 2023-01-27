833ae42d690db67454240ebad2aec4e252753fad
Brittany Burton <114821667+brittanyb89@users.noreply.github.com>
2023-01-26 19:19:24 -0600

    Create book model

---
+++ models/Book.js
@@ -0,0 +1,51 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Book extends Model {}
+
+Book.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    isbn: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    pages: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    reader_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "reader",
+        key: "id",
+      },
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "book",
+  }
+);
+
+module.exports = Book;
