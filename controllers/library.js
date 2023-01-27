2973916a62798b6d39e7635a97c7c7935b6970a6
Eli Baynes <sbaynes1994@gmail.com>
2023-01-26 20:32:13 -0600

    add files for openlibrary book search handling

---
+++ controllers/library.js
@@ -0,0 +1,1 @@
+// takes search parameters from search.js and ties them into an open library api call. Sends data to a handlebars template to be displayed on the page.
---
+++ public/js/search.js
@@ -0,0 +1,1 @@
+// pulls information from a search bar to send to the api folder to be used in an open library api call
