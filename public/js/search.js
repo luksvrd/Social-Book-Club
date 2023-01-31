a54dc17b5cded7ad37a37b6394b42dad6a47718b
Eli Baynes <sbaynes1994@gmail.com>
2023-01-30 21:43:14 -0600

    WIP search form handler

--- public/js/search.js
+++ public/js/search.js
@@ -1,29 +1,32 @@
-// pulls search paramaters from the search bar and performs a fetch request to openlibrary.org
-const searchFormHandler = async (event) => {
-  event.preventDefault();
+// form submit event listener to get search parameters and make a request to openlibrary.org
+document
+  .getElementById("search-form")
+  .addEventListener("submit", async (event) => {
+    event.preventDefault();
 
-  // base url for openlibrary.org
-  const OpenLibBaseURL = "https://openlibrary.org/search.json?";
+    // base url for openlibrary.org
+    const OpenLibBaseURL = "https://openlibrary.org/search.json?";
 
-  // get search parameters from the search bar fields
-  const title = document.querySelector("book-name").value.trim();
-  const author = document.querySelector("author").value.trim();
+    // get search parameters from the search bar fields
+    const title = document.getElementById("book-name").value.trim();
+    const author = document.getElementById("author").value.trim();
 
-  // build the url for the request based on not null search parameters
-  let url = OpenLibBaseURL;
-  if (title && !author) {
-    url += `title=${title}`;
-  }
-  if (author && !title) {
-    url += `author=${author}`;
-  }
-  if (title && author) {
-    url += `title=${title}` + `&author=${author}`;
-  }
+    // build the url for the request based on not null search parameters
+    let url = OpenLibBaseURL;
+    if (title && !author) {
+      url += `title=${title}`;
+    }
+    if (author && !title) {
+      url += `author=${author}`;
+    }
+    if (title && author) {
+      url += `title=${title}` + `&author=${author}`;
+    }
+    console.log(url);
 
-  // fetch request to openlibrary.org
-  const response = await fetch(url);
-  const data = await response.json();
+    // fetch request to openlibrary.org
+    const response = await fetch(url);
+    const data = await response.json();
 
-  console.log(data);
-};
+    console.log(data);
+  });
