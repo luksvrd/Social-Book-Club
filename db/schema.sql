b1903c398c514141251c02f595e926a375ed4309
luksvrd <ljvy9b@gmail.com>
2023-01-30 19:26:57 -0600

    Server working!

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,11 @@
 DROP DATABASE IF EXISTS books_db;
 CREATE DATABASE books_db;
 
-DROP DATABASE IF EXISTS users_db;
-CREATE DATABASE users_db;
+-- DROP DATABASE IF EXISTS users_db;
+-- CREATE DATABASE users_db;
 
-DROP DATABASE IF EXISTS comments_db;
-CREATE DATABASE comments_db;
+-- DROP DATABASE IF EXISTS comments_db;
+-- CREATE DATABASE comments_db;
 
-DROP DATABASE IF EXISTS bookshelves_db;
-CREATE DATABASE bookshelves_db;
+-- DROP DATABASE IF EXISTS bookshelves_db;
+-- CREATE DATABASE bookshelves_db;
