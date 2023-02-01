fde7a2d040d6cd1fa4f3715d27792aacf468733b
Eli Baynes <sbaynes1994@gmail.com>
2023-01-31 21:03:43 -0600

    add looper to create book objects from results

--- public/js/search.js
+++ public/js/search.js
@@ -1,3 +1,6 @@
+// array to store search results
+const searchResults = [];
+
 // form submit event listener to get search parameters and make a request to openlibrary.org
 document
   .getElementById("search-form")
@@ -27,5 +30,15 @@ document
     const response = await fetch(url);
     const data = await response.json();
 
-    console.log(data);
+    // create book object from the first 10 results
+    for (let i = 0; i < 10; i++) {
+      // book object
+      const book = {
+        title: data.docs[i].title,
+        author: data.docs[i].author_name[0],
+        isbn: data.docs[i].isbn[0],
+      };
+      // push book object to searchResults array
+      searchResults.push(book);
+    }
   });
