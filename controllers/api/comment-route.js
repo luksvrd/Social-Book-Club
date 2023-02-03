b12d200585a62ee7f646f407a0aae7bfc1174f75
luksvrd <ljvy9b@gmail.com>
2023-02-02 23:57:41 -0600

    add book routes, remove profile.js,  hbs comments

--- controllers/api/comment-route.js
+++ controllers/api/comment-route.js
@@ -2,31 +2,34 @@ const router = require("express").Router();
 const withAuth = require("../../utils/auth");
 const { Comment } = require("../../models");
 
+// Add a route to post a comment on a book and associate it with the user who posted it and the book it was posted on
+// route: /api/comments
 router.post("/", withAuth, async (req, res) => {
   try {
     const newComment = await Comment.create({
-      ...req.body,
+      // Adding a comment requires the user's id, the book's id, and the comment's text
       user_id: req.session.user_id,
+      book_id: req.body.book_id,
+      comment_text: req.body.comment_text,
     });
-    res.json(newComment);
+    res.status(200).json(newComment);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
+// Add route to delete a comment and remove it from the database
+// route: /api/comments/:id
 router.delete("/:id", withAuth, async (req, res) => {
   try {
-    const [affectedRows] = await Comment.destroy({
-      where: { id: req.params.id },
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+      },
     });
-
-    if (affectedRows > 0) {
-      res.status(200).end();
-    } else {
-      res.status(404).end();
-    }
+    res.status(200).json(commentData);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(400).json(err);
   }
 });
 
