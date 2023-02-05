7e0eb76c9964c7996d81df8196baf5be6bd198ce
Eli Baynes <sbaynes1994@gmail.com>
2023-02-04 19:58:03 -0600

    remove links to removed comment route file

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,9 @@
 const router = require("express").Router();
 const userRoutes = require("./user-route.js");
-const commentRoutes = require("./comment-route.js");
 const books = require("./books-route.js");
 const bookshelf = require("./bookshelf-route.js");
 
 router.use("/user", userRoutes);
-router.use("/comments", commentRoutes);
 router.use("/books", books);
 router.use("/bookshelf", bookshelf);
 
