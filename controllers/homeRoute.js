b9f338eb084d8708ab68d6a2bd2bce6ba0c800c2
luksvrd <ljvy9b@gmail.com>
2023-02-06 15:44:33 -0600

    homeroutes

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,46 +1,45 @@
-const { Bookshelf, User, Books } = require("../models");
+// const { Bookshelf, User, Books } = require("../models");
 const router = require("express").Router();
 // const withAuth = require("../utils/auth");
 
 // Get all bookshelves for homepage
-router.get("/", async (req, res) => {
-  console.log("hello");
-  try {
-    // Get all projects and JOIN with user data
-    const bookshelfData = await Bookshelf.findAll({
-      include: [
-        {
-          model: User,
-          attributes: ["name"],
-        },
-      ],
-    });
+// router.get("/", async (req, res) => {
+//   try {
+//     // Get all projects and JOIN with user data
+//     const bookshelfData = await Bookshelf.findAll({
+//       include: [
+//         {
+//           model: User,
+//           attributes: ["name"],
+//         },
+//       ],
+//     });
 
-    // Serialize data so the template can read it
-    const bookshelf = bookshelfData.map((bookshelf) =>
-      bookshelf.get({ plain: true })
-    );
+//     // Serialize data so the template can read it
+//     const bookshelf = bookshelfData.map((bookshelf) =>
+//       bookshelf.get({ plain: true })
+//     );
 
-    // Pass serialized data and session flag into template
-    // res.render("homepage", { previously
-    res.render("profile", {
-      bookshelf,
-      logged_in: req.session.loggedIn,
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     // Pass serialized data and session flag into template
+//     // res.render("homepage", { previously
+//     res.render("profile", {
+//       bookshelf,
+//       logged_in: req.session.loggedIn,
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-// Add aroute to get all the books in the database and render the homepage
-router.get("/", async (req, res) => {
-  try {
-    const bookData = await Books.findAll();
-    res.status(200).json(bookData);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// // Add aroute to get all the books in the database and render the homepage
+// router.get("/", async (req, res) => {
+//   try {
+//     const bookData = await Books.findAll();
+//     res.status(200).json(bookData);
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 // signup
 router.get("/signup", (req, res) => {
