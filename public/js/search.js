e57aa2f046a2876e84d94175f1a34b18141aa207
Eli Baynes <sbaynes1994@gmail.com>
2023-02-06 16:31:15 -0600

    change styling on book cards

--- public/js/search.js
+++ public/js/search.js
@@ -86,14 +86,13 @@ document
       const card = document.createElement("div");
       card.classList.add("card", "gap-y-2", "border-sm", "border-black");
       card.innerHTML = `
-<div class="grid grid-cols-2 gap-4 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-<a href="#">
+<div class="grid grid-cols-2 gap-4 max-w-sm bg-gradient-to-r from-orange-100 to-orange-200 border border-black rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-1">
     <img class="p-4 rounded-t-lg" src="${book.cover}" alt="" />
 </a>
 <div class="p-5 grid grid-flow-row auto-rows-max">
     <p class="card-title mb-3 font-bold text-gray-700 dark:text-gray-400">${book.title}</p>
     <p class="card-subtitle inline-flex items-center p-2 text-sm font-medium text-center text-gray-700">${book.author}</p>
-    <p class="card-text inline-flex items-center p-2 text-sm font-medium text-center text-gray-700">${book.isbn}</p>
+    <p class=" card-text hidden items-center p-2 text-sm font-medium text-center text-gray-700">${book.isbn}</p>
     <button class="card-button inline-flex items-center p-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
     Add to Bookshelf </button>
 </div>
